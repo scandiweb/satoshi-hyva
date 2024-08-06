32587e12746c814fb152fb995ff29a3e29e1c293
Nika Beridze <nika.beridze@scandiweb.com>
2024-08-06 04:23:07 +0400

    INTERNAL-58: Change size of product cart and add ProductsDetails block

--- app/code/Satoshi/Theme/Block/Template.php
+++ app/code/Satoshi/Theme/Block/Template.php
@@ -8,24 +8,24 @@ use Magento\Framework\View\Element\Template\Context;
 class Template extends MagentoTemplate
 {
 
-  public function __construct(
-    Context $context,
-    array $data = []
-  ) {
-    parent::__construct($context, $data);
-  }
+    public function __construct(
+        Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
 
-  public function setChildTemplate(string $childTemplate)
-  {
-    $this->setChild(
-      'child-content',
-      $this->getLayout()->createBlock(Template::class)->setTemplate($childTemplate)
-    );
-    return $this;
-  }
+    public function setChildTemplate(string $childTemplate)
+    {
+        $this->setChild(
+            'child-content',
+            $this->getLayout()->createBlock(Template::class)->setTemplate($childTemplate)
+        );
+        return $this;
+    }
 
-  public function render(string $template)
-  {
-    return $this->setTemplate($template)->toHtml();
-  }
+    public function render(string $template)
+    {
+        return $this->setTemplate($template)->toHtml();
+    }
 }
