0454d1eab06316a701e5f911ba67d3de7cb07b6e
Mohamed Idris <mohamed.idris@scandiweb.com>
2024-08-13 22:41:23 +0300

    INTERNAL-53: refactored the LanguageSelector ViewModel to avoid using App Emulation when retrieving store locale information

--- app/code/Satoshi/Theme/ViewModel/LanguageSelector.php
+++ app/code/Satoshi/Theme/ViewModel/LanguageSelector.php
@@ -6,22 +6,18 @@ use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\Framework\Locale\ResolverInterface;
-use Magento\Store\Model\App\Emulation;
 
 class LanguageSelector implements ArgumentInterface
 {
     protected StoreManagerInterface $storeManager;
     protected ResolverInterface $localeResolver;
-    protected Emulation $appEmulation;
 
     public function __construct(
         StoreManagerInterface $storeManager,
-        ResolverInterface $localeResolver,
-        Emulation $appEmulation
+        ResolverInterface $localeResolver
     ) {
         $this->storeManager = $storeManager;
         $this->localeResolver = $localeResolver;
-        $this->appEmulation = $appEmulation;
     }
 
     public function getAvailableLanguages(): array
@@ -29,21 +25,13 @@ class LanguageSelector implements ArgumentInterface
         $stores = $this->storeManager->getStores();
         $languages = [];
         foreach ($stores as $store) {
-            // Start store emulation
-            $this->appEmulation->startEnvironmentEmulation($store->getId());
+            $scopeLocale = $this->localeResolver->getLocale($store->getId());
 
-            // Get the locale for the store
-            $localeCode = $this->localeResolver->getLocale();
-
-            // Add store data to the languages array
             $languages[$store->getCode()] = [
                 'name' => $store->getName(),
-                'locale' => $localeCode,
+                'locale' => $scopeLocale,
                 'url' => $store->getBaseUrl()
             ];
-
-            // Stop store emulation
-            $this->appEmulation->stopEnvironmentEmulation();
         }
         return $languages;
     }
