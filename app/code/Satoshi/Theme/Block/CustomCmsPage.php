0c82dee8cf50f9b913bd02551bd20f37e51ee68a
Mohamed Idris <mohamed.idris@scandiweb.com>
2024-08-19 14:22:29 +0300

    INTERNAL-4: add a preference in di.xml to replace the core Magento\Cms\Block\Page with Satoshi\Theme\Block\CustomCmsPage, and remove padding in page.phtml file

--- app/code/Satoshi/Theme/Block/CustomCmsPage.php
+++ app/code/Satoshi/Theme/Block/CustomCmsPage.php
@@ -2,34 +2,40 @@
 
 namespace Satoshi\Theme\Block;
 
-use Magento\Cms\Block\Page as CorePage;
-use Magento\Cms\Model\Page;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 use Magento\Cms\Model\Template\FilterProvider;
 use Magento\Cms\Model\PageFactory;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\Framework\View\Page\Config;
+use Magento\Cms\Model\Page as CmsPageModel;
 
-class CustomCmsPage extends CorePage
+class CustomCmsPage extends Template
 {
+    protected $_filterProvider;
+    protected $_pageFactory;
+    protected $_storeManager;
+    protected $pageConfig;
+    protected $_scopeConfig;
+    protected $_page;
+
     public function __construct(
         Context $context,
         FilterProvider $filterProvider,
         StoreManagerInterface $storeManager,
         PageFactory $pageFactory,
         Config $pageConfig,
+        CmsPageModel $page,
         array $data = []
     ) {
-        parent::__construct(
-            $context,
-            $pageFactory->create(),
-            $filterProvider,
-            $storeManager,
-            $pageFactory,
-            $pageConfig,
-            $data
-        );
+        parent::__construct($context, $data);
+        $this->_filterProvider = $filterProvider;
+        $this->_storeManager = $storeManager;
+        $this->_pageFactory = $pageFactory;
+        $this->pageConfig = $pageConfig;
+        $this->_scopeConfig = $context->getScopeConfig();
+        $this->_page = $page;
     }
 
     public function getPageId()
@@ -40,7 +46,13 @@ class CustomCmsPage extends CorePage
     /**
      * Retrieve the CMS Page instance.
      *
-     * @return Page
+     * This method loads the CMS page based on the current store and page identifier.
+     * It ensures that the page data is set only once by checking if it already exists.
+     *
+     * The original method from the core class didn't work because it relied on the
+     * default identifier 'home', which didn't match the expected behavior for our custom setup.
+     *
+     * @return CmsPageModel
      * @throws NoSuchEntityException
      */
     public function getPage()
@@ -53,5 +65,62 @@ class CustomCmsPage extends CorePage
         }
 
         return $this->getData('page');
+    }
+
+    public function getPageTitle()
+    {
+        return $this->getPage()->getTitle();
+    }
+
+    public function getPageContent()
+    {
+        return $this->_filterProvider->getPageFilter()->filter($this->getPage()->getContent());
+    }
+
+    protected function _prepareLayout()
+    {
+        $page = $this->getPage();
+        $this->_addBreadcrumbs($page);
+        $this->pageConfig->addBodyClass('cms-' . $page->getIdentifier());
+        $metaTitle = $page->getMetaTitle();
+        $this->pageConfig->getTitle()->set($metaTitle ? $metaTitle : $page->getTitle());
+        $this->pageConfig->setKeywords($page->getMetaKeywords());
+        $this->pageConfig->setDescription($page->getMetaDescription());
+
+        $pageMainTitle = $this->getLayout()->getBlock('page.main.title');
+        if ($pageMainTitle) {
+            $cmsTitle = $page->getContentHeading() ?: ' ';
+            $pageMainTitle->setPageTitle($this->escapeHtml($cmsTitle));
+        }
+        return parent::_prepareLayout();
+    }
+
+    protected function _addBreadcrumbs(CmsPageModel $page)
+    {
+        $homePageIdentifier = $this->_scopeConfig->getValue('web/default/cms_home_page', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        $homePageDelimiterPosition = $homePageIdentifier === null ? false : strrpos($homePageIdentifier, '|');
+        if ($homePageDelimiterPosition) {
+            $homePageIdentifier = substr($homePageIdentifier, 0, $homePageDelimiterPosition);
+        }
+        $noRouteIdentifier = $this->_scopeConfig->getValue('web/default/cms_no_route', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        $noRouteDelimiterPosition = $noRouteIdentifier === null ? false : strrpos($noRouteIdentifier, '|');
+        if ($noRouteDelimiterPosition) {
+            $noRouteIdentifier = substr($noRouteIdentifier, 0, $noRouteDelimiterPosition);
+        }
+        if ($this->_scopeConfig->getValue('web/default/show_cms_breadcrumbs', \Magento\Store\Model\ScopeInterface::SCOPE_STORE)
+            && ($breadcrumbsBlock = $this->getLayout()->getBlock('breadcrumbs'))
+            && $page->getIdentifier() !== $homePageIdentifier
+            && $page->getIdentifier() !== $noRouteIdentifier
+        ) {
+            $breadcrumbsBlock->addCrumb(
+                'home',
+                [
+                    'label' => __('Home'),
+                    'title' => __('Go to Home Page'),
+                    'link' => $this->_storeManager->getStore()->getBaseUrl()
+                ]
+            );
+            $breadcrumbsBlock->addCrumb('cms_page', ['label' => $page->getTitle(), 'title' => $page->getTitle()]);
+        }
     }
 }
