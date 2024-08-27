bdba4da4263d62e0e417b9e0707d8ec54acdf8a3
Mohamed Idris <mohamed.idris@scandiweb.com>
2024-08-27 23:17:03 +0300

    INTERNAL-53; refactor URL logic, and rename variables to 'store' instead of 'language'

--- app/code/Satoshi/Theme/ViewModel/StoreSelector.php
+++ app/code/Satoshi/Theme/ViewModel/StoreSelector.php
@@ -1,19 +1,19 @@
 <?php
-
 declare(strict_types=1);
 
 namespace Satoshi\Theme\ViewModel;
 
+use Magento\Framework\Escaper;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\Framework\Locale\ResolverInterface;
 
 /**
- * ViewModel for Language Selector.
+ * ViewModel for Store Selector.
  *
- * This ViewModel provides functionality to fetch available languages
- * and their respective store information for the language selector component.
+ * This ViewModel provides functionality to fetch available stores
+ * and their respective store information for the store selector component.
  */
 class StoreSelector implements ArgumentInterface
 {
@@ -28,64 +28,80 @@ class StoreSelector implements ArgumentInterface
     protected ResolverInterface $localeResolver;
 
     /**
+     * @var Escaper
+     */
+    protected Escaper $escaper;
+
+    /**
      * StoreSelector constructor.
      *
      * @param StoreManagerInterface $storeManager
      * @param ResolverInterface $localeResolver
+     * @param Escaper $escaper
      */
     public function __construct(
         StoreManagerInterface $storeManager,
-        ResolverInterface $localeResolver
+        ResolverInterface $localeResolver,
+        Escaper $escaper,
     ) {
         $this->storeManager = $storeManager;
         $this->localeResolver = $localeResolver;
+        $this->escaper = $escaper;
     }
 
     /**
      * Retrieve available stores and their details.
      *
      * This method returns an array where each entry corresponds to a store
-     * with its code, name, locale, and base URL.
+     * with its code, name, locale, and URL.
      *
      * @return array<string, array{name: string, locale: string, url: string}>
      */
     public function getAvailableStores(): array
     {
         $stores = $this->storeManager->getStores();
+        $defaultStoreCode = $this->getDefaultStoreCode();
         $storesInfo = [];
+
         foreach ($stores as $store) {
             $scopeLocale = $this->localeResolver->getLocale($store->getId());
+            $url = $store->getBaseUrl();
+            $storeCode = $store->getCode();
 
-            $storesInfo[$store->getCode()] = [
+            if ($storeCode !== $defaultStoreCode) {
+                $url .= '?___store=' . $this->escaper->escapeJs($storeCode);
+            }
+
+            $storesInfo[$storeCode] = [
                 'name' => $store->getName(),
                 'locale' => $scopeLocale,
-                'url' => $store->getBaseUrl()
+                'url' => $url
             ];
         }
         return $storesInfo;
     }
 
     /**
-     * Retrieve the current language code.
+     * Retrieve the current store code.
      *
      * This method returns the store code of the current store.
      *
      * @return string
      * @throws NoSuchEntityException If the store is not found.
      */
-    public function getCurrentLanguageCode(): string
+    public function getCurrentStoreCode(): string
     {
         return $this->storeManager->getStore()->getCode();
     }
 
     /**
-     * Get the default language code.
+     * Get the default store code.
      *
      * This method returns the store code of the default store view.
      *
      * @return string
      */
-    public function getDefaultLanguageCode(): string
+    public function getDefaultStoreCode(): string
     {
         $defaultStore = $this->storeManager->getDefaultStoreView();
         return $defaultStore->getCode();
