769a7bafc3398ab0a74efe9dff9f0363731f7a1b
Mohamed Idris <mohamed.idris@scandiweb.com>
2024-09-27 23:24:13 +0300

    INTERNAL-17; prevent page reloading and disable btn on click

--- app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/AccountInformation.ts
+++ app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/AccountInformation.ts
@@ -2,33 +2,40 @@ import { Magics } from "alpinejs";
 import { navigateWithTransition } from "../plugins/Transition";
 
 export type AccountInformationType = {
+    isSaving: boolean;
     fetchAndReplaceContent(url: string): void;
     updateAccount(): void;
 } & Magics<{}>;
 
 export const AccountInformation = () =>
     <AccountInformationType>{
-        fetchAndReplaceContent(url: string) {
-            if (!url) return;
+    isSaving: false,
 
-            navigateWithTransition(url);
-        },
+    fetchAndReplaceContent(url: string) {
+        if (!url) return;
+        navigateWithTransition(url);
+    },
 
-        updateAccount() {
-            const form = document.getElementById('form-validate') as HTMLFormElement;
-            if (!form) return;
-            const formData = new FormData(form);
+    updateAccount() {
+        const form = document.getElementById('form-validate') as HTMLFormElement;
+        if (!form) return;
 
-            fetch("/customer/account/editPost", {
-                method: "POST",
-                body: formData,
-            }).then((res) => {
-                if (res.ok) {
-                    navigateWithTransition('/customer/account');
-                }
-            }).catch((error) => {
-                console.error("Error while updating account information:", error);
-                location.reload();
-            });
-        },
-    };
+        const formData = new FormData(form);
+        this.isSaving = true;
+
+        fetch("/customer/account/editPost", {
+            method: "POST",
+            body: formData,
+        }).then((res) => {
+            if (res.ok) {
+                navigateWithTransition('/customer/account');
+            }
+        }).catch((error) => {
+            console.error("Error while updating account information:", error);
+            location.reload();
+        }).finally(() => {
+            this.isSaving = false;
+        });
+
+    },
+};
