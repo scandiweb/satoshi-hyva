8fb52992bb658485b45b4fc9f29465a4efdd68b6
Mahmoud Shiref <mahmoud.shiref@scandiweb.com>
2024-09-27 11:29:17 +0300

    INTERNAL-37: Revert unchanged files

--- app/code/Satoshi/Theme/Block/Popup.php
+++ app/code/Satoshi/Theme/Block/Popup.php
@@ -8,36 +8,35 @@ use Magento\Framework\View\Element\Text;
 
 class Popup extends Template
 {
-    protected $_template = 'Satoshi_Theme::popup.phtml';
+  protected $_template = 'Satoshi_Theme::popup.phtml';
 
-    public function __construct(
-        Context $context,
-        array   $data = []
-    )
-    {
-        parent::__construct($context, $data);
-    }
+  public function __construct(
+    Context $context,
+    array $data = []
+  ) {
+    parent::__construct($context, $data);
+  }
 
-    public function setChildTemplate(string $childTemplate)
-    {
-        $this->setChild(
-            'popup-content',
-            $this->getLayout()->createBlock(Template::class)->setTemplate($childTemplate)
-        );
-        return $this;
-    }
+  public function setChildTemplate(string $childTemplate)
+  {
+    $this->setChild(
+      'popup-content',
+      $this->getLayout()->createBlock(Template::class)->setTemplate($childTemplate)
+    );
+    return $this;
+  }
 
-    public function setChildHtml(string $childHtml)
-    {
-        $this->setChild(
-            'popup-content',
-            $this->getLayout()->createBlock(Text::class)->setText($childHtml)
-        );
-        return $this;
-    }
+  public function setChildHtml(string $childHtml)
+  {
+    $this->setChild(
+      'popup-content',
+      $this->getLayout()->createBlock(Text::class)->setText($childHtml)
+    );
+    return $this;
+  }
 
-    public function render()
-    {
-        return $this->toHtml();
-    }
+  public function render()
+  {
+    return $this->toHtml();
+  }
 }
