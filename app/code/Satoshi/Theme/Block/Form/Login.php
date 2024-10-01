45c3b5f1f97c1a69b1475587dd129eb661af41e4
Nika Beridze <nika.beridze@scandiweb.com>
2024-10-01 23:37:26 +0400

    INTERNAL-46: Implement block parent inheritance and change location of ForgotPasswordPost

--- app/code/Satoshi/Theme/Block/Form/Login.php
+++ app/code/Satoshi/Theme/Block/Form/Login.php
@@ -2,92 +2,33 @@
 
 namespace Satoshi\Theme\Block\Form;
 
-/**
- * Customer login form block
- *
- * @api
- * @author      Magento Core Team <core@magentocommerce.com>
- * @since 100.0.2
- */
-class Login extends \Magento\Framework\View\Element\Template
+use Magento\Customer\Block\Form\Login as SourceLogin;
+use Magento\Customer\Model\Session;
+use Magento\Customer\Model\Url;
+use Magento\Framework\View\Element\Template\Context;
+
+
+class Login extends SourceLogin
 {
     /**
-     * @var int
-     */
-    private $_username = -1;
-
-    /**
-     * @var \Magento\Customer\Model\Session
+     * @var Session
      */
     protected $_customerSession;
 
     /**
-     * @var \Magento\Customer\Model\Url
-     */
-    protected $_customerUrl;
-
-    /**
-     * @param \Magento\Framework\View\Element\Template\Context $context
-     * @param \Magento\Customer\Model\Session $customerSession
-     * @param \Magento\Customer\Model\Url $customerUrl
+     * @param Context $context
+     * @param Session $customerSession
+     * @param Url $customerUrl
      * @param array $data
      */
     public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        \Magento\Customer\Model\Session $customerSession,
-        \Magento\Customer\Model\Url $customerUrl,
+        Context $context,
+        Session $customerSession,
+        Url $customerUrl,
         array $data = []
     ) {
-        parent::__construct($context, $data);
-        $this->_isScopePrivate = false;
-        $this->_customerUrl = $customerUrl;
         $this->_customerSession = $customerSession;
-    }
-
-    /**
-     * Retrieve form posting url
-     *
-     * @return string
-     */
-    public function getPostActionUrl()
-    {
-        return $this->_customerUrl->getLoginPostUrl();
-    }
-
-    /**
-     * Retrieve password forgotten url
-     *
-     * @return string
-     */
-    public function getForgotPasswordUrl()
-    {
-        return $this->_customerUrl->getForgotPasswordUrl();
-    }
-
-    /**
-     * Retrieve username for form field
-     *
-     * @return string
-     */
-    public function getUsername()
-    {
-        if (-1 === $this->_username) {
-            $this->_username = $this->_customerSession->getUsername(true);
-        }
-        return $this->_username;
-    }
-
-    /**
-     * Check if autocomplete is disabled on storefront
-     *
-     * @return bool
-     */
-    public function isAutocompleteDisabled()
-    {
-        return (bool)!$this->_scopeConfig->getValue(
-            \Magento\Customer\Model\Form::XML_PATH_ENABLE_AUTOCOMPLETE,
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-        );
+        parent::__construct($context, $customerSession, $customerUrl, $data);
     }
 
     /**
---
+++ app/code/Satoshi/Theme/Customer/Controller/Account/ForgotPasswordPost.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Satoshi\Theme\Customer\Controller\Account;
+
+use Magento\Customer\Api\AccountManagementInterface;
+use Magento\Customer\Controller\Account\ForgotPasswordPost as SourceForgotPasswordPost;
+use Magento\Customer\Model\AccountManagement;
+use Magento\Customer\Model\Session;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\Result\JsonFactory;
+use Magento\Framework\Controller\Result\Redirect;
+use Magento\Framework\Escaper;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Exception\SecurityViolationException;
+use Magento\Framework\Validator\EmailAddress;
+use Magento\Framework\Validator\ValidatorChain;
+
+class ForgotPasswordPost extends SourceForgotPasswordPost
+{
+    protected JsonFactory $jsonFactory;
+    protected $session;
+
+    public function __construct(
+        Context                    $context,
+        Session                    $customerSession,
+        AccountManagementInterface $customerAccountManagement,
+        Escaper                    $escaper,
+        JsonFactory                $jsonFactory,
+    )
+    {
+        $this->jsonFactory = $jsonFactory;
+        $this->session = $customerSession;
+        parent::__construct($context, $customerSession, $customerAccountManagement, $escaper);
+    }
+
+    public function execute()
+    {
+        /** @var Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $email = (string)$this->getRequest()->getPost('email');
+        if ($email) {
+            if (!ValidatorChain::is($email, EmailAddress::class)) {
+                $this->session->setForgottenEmail($email);
+                // Store error message in session
+                $this->session->setErrorMessage(__('The email address is incorrect. Verify the email address and try again.'));
+                return $resultRedirect->setPath('*/*/forgotpassword');
+            }
+
+            try {
+                $this->customerAccountManagement->initiatePasswordReset(
+                    $email,
+                    AccountManagement::EMAIL_RESET
+                );
+                // phpcs:ignore Magento2.CodeAnalysis.EmptyBlock.DetectedCatch
+            } catch (NoSuchEntityException $exception) {
+                // Do nothing, we don't want anyone to use this action to determine which email accounts are registered.
+            } catch (SecurityViolationException $exception) {
+                $this->session->setErrorMessage($exception->getMessage());
+                return $resultRedirect->setPath('*/*/forgotpassword');
+            } catch (\Exception $exception) {
+                $this->session->setErrorMessage(__('We\'re unable to send the password reset email.'));
+                return $resultRedirect->setPath('*/*/forgotpassword');
+            }
+            $this->session->setSuccessMessage($this->getSuccessMessage($email));
+            return $resultRedirect->setPath('*/*/');
+        } else {
+            $this->session->setErrorMessage(__('Please enter your email.'));
+            return $resultRedirect->setPath('*/*/forgotpassword');
+        }
+    }
+}
