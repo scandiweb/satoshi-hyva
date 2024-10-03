3a11870d874b152f28c502d09057b5dc39073fb5
Mohamed Idris <mohamed.idris@scandiweb.com>
2024-10-03 14:33:04 +0300

    INTERNAL-48; add a comment about pagination logic

--- app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/Register.ts
+++ app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/Register.ts
@@ -23,6 +23,9 @@ export const Register = () =>
             }).then((res) => {
                 if (res.ok) {
                     navigateWithTransition('/customer/account');
+                    // After merging the INTERNAL-53, https://github.com/satoshiux/hyva/pull/19/files
+                    // we can use the line below instead of the line above
+                    // navigateWithTransition('/customer/account', { replaceDocument: true });
                 }
             }).catch((error) => {
                 console.error("Error while registering account:", error);
