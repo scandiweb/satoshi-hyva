282522f3465abb11afb06315e2ee01bf29046cb2
Nika Beridze <nika.beridze@scandiweb.com>
2024-10-06 07:16:21 +0400

    INTERNAL-56: Implement google recaptcha for login form in hyva way

--- app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/Authentication.ts
+++ app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/Authentication.ts
@@ -1,87 +1,85 @@
-import { replaceMainContentWithTransition } from "../plugins/Transition";
-import { MainStoreType } from "../store/Main";
+import {replaceMainContentWithTransition} from "../plugins/Transition";
+import {MainStoreType} from "../store/Main";
 
 export type AuthenticationType = {
-  errors: number;
-  hasCaptchaToken: boolean;
-  showPassword: boolean;
-  displayErrorMessage: boolean;
-  errorMessages: Record<string, string>;
-  generalErrorMessage: string;
+    displayErrorMessage: boolean;
+    errorMessages: Record<string, string>;
+    generalErrorMessage: string;
 
-  init(): void;
-  clearError(field: string): void;
-  setErrorMessages(messages: Record<string, string>): void;
-  submitForm(event: Event, formId: string): void;
-  submitAuthForm(formId: string): void;
+    clearError(field: string): void;
+    setErrorMessages(messages: Record<string, string>): void;
+    submitForm(event: Event, formId: string): void;
+    submitAuthForm(formId: string): void;
 } & MainStoreType;
 
 export const Authentication = () => {
-  return <AuthenticationType>{
-    errors: 0,
-    hasCaptchaToken: false,
-    showPassword: false,
-    displayErrorMessage: false,
-    errorMessages: {},
-    generalErrorMessage: "",
+    return <AuthenticationType>{
+        displayErrorMessage: false,
+        errorMessages: {},
+        generalErrorMessage: "",
 
-    clearError(field) {
-      this.errorMessages[field] = "";
-    },
-
-    setErrorMessages(messages) {
-      this.errorMessages = messages;
-      this.displayErrorMessage = true;
-    },
-
-    submitForm(event, formId) {
-      event.preventDefault();
-
-      const form = document.querySelector(`#${formId}`) as HTMLFormElement;
-      if (!form) {
-        return;
-      }
-
-      fetch(form.action, {
-        method: "POST",
-        body: new FormData(form),
-        headers: {
-          "X-Requested-With": "XMLHttpRequest",
+        clearError(field) {
+            this.errorMessages[field] = "";
         },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.success) {
-            form.submit();
-          } else {
-            this.setErrorMessages({ [data.field]: data.message });
-          }
-        })
-        .catch((error) => {
-          this.generalErrorMessage = error.message();
-          this.displayErrorMessage = true;
-        });
-    },
 
-    submitAuthForm(formId) {
-      const form = document.getElementById(formId) as HTMLFormElement;
-      const formData = new FormData(form);
-
-      fetch(form.action, {
-        method: "POST",
-        body: formData,
-        headers: {
-          "X-Requested-With": "XMLHttpRequest",
+        setErrorMessages(messages) {
+            this.errorMessages = {...this.errorMessages, ...messages};
+            this.displayErrorMessage = true;
         },
-      })
-        .then((response) => {
-          return response.text().then(async (content) => {
-            await replaceMainContentWithTransition(response.url, content);
-          });
-        })
-        .catch((error) => {
-          console.error("Form submission failed:", error);
-        });
-    },
-  };
+
+        submitForm(event, formId) {
+            event.preventDefault();
+
+            const form = document.querySelector(`#${formId}`) as HTMLFormElement;
+            if (!form) {
+                return;
+            }
+
+            fetch(form.action, {
+                method: "POST",
+                body: new FormData(form),
+                headers: {
+                    "X-Requested-With": "XMLHttpRequest",
+                },
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    if (data.success) {
+                        form.submit();
+                    } else {
+                        this.setErrorMessages({[data.field]: data.message});
+                    }
+                })
+                .catch((error) => {
+                    this.generalErrorMessage = error.message();
+                    this.displayErrorMessage = true;
+                });
+        },
+
+        submitAuthForm(formId, recaptchaValidationHtml?: string) {
+            const $form = document.getElementById(formId) as HTMLFormElement;
+            eval(recaptchaValidationHtml);
+            if (this.errors === 0) {
+                this.errorMessages = {};
+                const formData = new FormData($form);
+                fetch($form.action, {
+                    method: "POST",
+                    body: formData,
+                    headers: {
+                        "X-Requested-With": "XMLHttpRequest",
+                    },
+                })
+                    .then((response) => {
+                        return response.text().then(async (content) => {
+                            await replaceMainContentWithTransition(response.url, content);
+                        });
+                    })
+                    .catch((error) => {
+                        this.setErrorMessages({'request error': error.message});
+                    });
+            } else {
+                this.setErrorMessages({'recaptcha-error': 'Please check the box to verify you are not a robot'});
+            }
+        },
+    };
 };
