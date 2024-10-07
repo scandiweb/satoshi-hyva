6b7fe2f63a2ce6787e081c5be0bf08dba3dfcbd7
Mohamed Idris <mohamed.idris@scandiweb.com>
2024-10-07 14:06:02 +0300

    INTERNAL-17; refactor ts code

--- app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/AccountInformation.ts
+++ app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/AccountInformation.ts
@@ -3,15 +3,15 @@ import { navigateWithTransition } from "../plugins/Transition";
 
 export type AccountInformationType = {
     isLoading: boolean;
-    handleFormSubmit(): void;
+    handleFormSubmit(event: Event): void;
 } & Magics<{}>;
 
 export const AccountInformation = () =>
     <AccountInformationType>{
     isLoading: false,
 
-    handleFormSubmit() {
-        const $form = document.getElementById('form-validate') as HTMLFormElement;
+    handleFormSubmit(event) {
+        const $form = event.target as HTMLFormElement;
         if (!$form) return;
 
         const formData = new FormData($form);
@@ -22,7 +22,7 @@ export const AccountInformation = () =>
             body: formData,
         }).then((res) => {
             if (res.ok) {
-                navigateWithTransition('/customer/account');
+                navigateWithTransition($form.action);
             }
         }).catch((error) => {
             console.error("Error while updating account information:", error);
