5de34241c82d693028a42fab595209c02bbc3e0f
Mohamed Idris <mohamed.idris@scandiweb.com>
2024-10-07 16:12:58 +0300

    INTERNAL-17; move script tag inside ts file

--- app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/AccountInformation.ts
+++ app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/AccountInformation.ts
@@ -1,16 +1,53 @@
-import { Magics } from "alpinejs";
-import { navigateWithTransition } from "../plugins/Transition";
+import {Magics} from "alpinejs";
+import {replaceMainContentWithTransition} from "../plugins/Transition";
 
 export type AccountInformationType = {
-    isLoading: boolean;
-    handleFormSubmit(event: Event): void;
+  isLoading: boolean;
+  showEmailField: boolean;
+  showPasswordFields: boolean;
+  errorMessages: string[];
+  displayErrorMessage: boolean;
+  errors: number;
+  validate(): Promise<void>;
+  handleCheckboxChange(checkboxId: string): void;
+  setErrorMessages(messages: string[]): void;
+  saveChanges(event: Event): void;
+  submitForm(event: Event): void;
 } & Magics<{}>;
 
-export const AccountInformation = () =>
+export const AccountInformation = (
+    initialShowPasswordFields: boolean,
+) =>
     <AccountInformationType>{
-    isLoading: false,
+      isLoading: false,
+      showEmailField: false,
+      showPasswordFields: initialShowPasswordFields,
+      errorMessages: [] as string[],
+      displayErrorMessage: false,
+      errors: 0,
 
-    handleFormSubmit(event) {
+      handleCheckboxChange(checkboxId) {
+        if (checkboxId === 'email-fields') {
+          this.showEmailField = !this.showEmailField;
+        }
+        if (checkboxId === 'password-fields') {
+          this.showPasswordFields = !this.showPasswordFields;
+        }
+
+        this.$nextTick(() => {
+          const firstFocusableElement = document.querySelector(`#${checkboxId} input, #${checkboxId} textarea, #${checkboxId} select`) as HTMLElement;
+          if (firstFocusableElement) {
+            firstFocusableElement.focus();
+          }
+        });
+      },
+
+      setErrorMessages(messages: string[]) {
+        this.errorMessages = messages;
+        this.displayErrorMessage = messages.length > 0;
+      },
+
+      saveChanges(event) {
         const $form = event.target as HTMLFormElement;
         if (!$form) return;
 
@@ -18,17 +55,35 @@ export const AccountInformation = () =>
         this.isLoading = true;
 
         fetch($form.action, {
-            method: "POST",
-            body: formData,
-        }).then((res) => {
-            if (res.ok) {
-                navigateWithTransition($form.action);
-            }
+          method: "POST",
+          body: formData,
+        }).then(async (res) => {
+          if (res.ok) {
+            await replaceMainContentWithTransition(res.url, await res.text());
+          }
         }).catch((error) => {
-            console.error("Error while updating account information:", error);
-            location.reload();
+          console.error("Error while updating account information:", error);
+          location.reload();
         }).finally(() => {
-            this.isLoading = false;
+          this.isLoading = false;
         });
-    },
-};
+      },
+
+      submitForm(event) {
+        this.validate()
+            .then(() => {
+              // Do not rename $form, the variable is expected to be declared in the recaptcha output
+              // const $form = event.target;
+              // <?= $recaptcha ? $recaptcha->getValidationJsHtml(ReCaptcha::RECAPTCHA_FORM_ID_CUSTOMER_EDIT) : '' ?>
+
+              if (this.errors === 0) {
+                this.saveChanges(event);
+              }
+            })
+            .catch((invalid) => {
+              if (invalid.length > 0) {
+                (invalid[0] as HTMLElement).focus();
+              }
+            });
+      },
+    };
