3fe3204442a31624685acf343e020908c42324df
sh1r3f <sh1r3f@gmail.com>
2024-10-07 09:35:06 +0300

    INTERNAL-70: Review list improvements

--- app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/ReviewList.ts
+++ app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/ReviewList.ts
@@ -1,118 +1,132 @@
 import nProgress from "nprogress";
 
-nProgress.configure({showSpinner: false});
+nProgress.configure({ showSpinner: false });
 
 type Review = {
-    product: {
-        image: {
-            url: string;
-            label: string;
-        };
-        name: string;
-        url_key: string;
+  product: {
+    image: {
+      url: string;
+      label: string;
     };
-    created_at: string;
-    ratings_breakdown: { value: number }[];
-    summary: string;
-    text: string;
+    name: string;
+    url_key: string;
+  };
+  created_at: string;
+  ratings_breakdown: { value: number }[];
+  summary: string;
+  text: string;
 };
 
 type PageInfo = {
-    page_size: number;
-    total_pages: number;
-    current_page: number;
+  page_size: number;
+  total_pages: number;
+  current_page: number;
 };
 
-type AlpineReviewList = {
-    customerToken: string | false;
-    currentPage: number;
-    pageSize: number;
-    totalPagesObject: number[];
-    pageInfo: PageInfo | null;
-    reviews: Review[];
-    isLoading: boolean;
+type ReviewListType = {
+  customerToken: string | false;
+  currentPage: number;
+  pageSize: number;
+  totalPagesObject: any;
+  pageInfo: PageInfo | null | any;
+  reviews: Review[] | any;
+  isLoading: boolean;
+  errors: string[];
 
-    getCustomerReviewsQuery(): string;
-
-    getReviewsList(): void;
-
-    onPrivateContentLoaded(data: any): void;
-
-    initErrorMessages(errors: any): void;
+  onPrivateContentLoaded(data: any): void;
+  setCurrentPage(page: any): void;
+  getCustomerReviewsQuery(): string;
+  getReviewsList(append?: boolean): void;
+  initErrorMessages(errors: any): void;
 };
 
 export const ReviewList = (props: {
-    baseUrl: string;
-    productUrlSuffix: string;
-    storeCode: string;
-    customerReviewsQuery: string;
-}) => <AlpineReviewList>{
+  baseUrl: string;
+  productUrlSuffix: string;
+  storeCode: string;
+  customerReviewsQuery: string;
+}) =>
+  <ReviewListType>{
     customerToken: false,
     currentPage: 1,
-    pageSize: 10,
-    totalPagesObject: [],
-    pageInfo: null,
-    reviews: [],
-    errors: [],
+    pageSize: 5,
+    totalPagesObject: {},
+    pageInfo: {},
     isLoading: true,
+    reviews: {},
+    errors: [],
 
-    getCustomerReviewsQuery() {
-        return props.customerReviewsQuery
-            .replace('%currentPage%', this.currentPage.toString())
-            .replace('%pageSize%', this.pageSize.toString());
-    },
-    setPageSize() {
-        this.pageSize += 10;
+    onPrivateContentLoaded(data) {
+      this.customerToken = data.customer["signin_token"];
+      if (this.customerToken) {
         this.getReviewsList();
+      } else {
+        this.isLoading = false;
+        nProgress.done();
+      }
     },
-    getReviewsList() {
-        this.isLoading = true;
-        nProgress.start();
-        return fetch(`${props.baseUrl}graphql`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Store: props.storeCode
-            },
-            credentials: 'include',
-            body: JSON.stringify({
-                query: this.getCustomerReviewsQuery()
-            })
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                if (data && data.errors) {
-                    this.initErrorMessages(data.errors);
-                } else {
-                    const {
-                        data: {
-                            customer: {
-                                reviews: {
-                                    items = [],
-                                    page_info = {},
-                                } = {}
-                            } = {}
-                        } = {}
-                    } = data || {};
-                    this.reviews = items;
-                    this.pageInfo = page_info;
-                    this.totalPagesObject = Array.from({length: this.pageInfo?.total_pages || 0}, (_, i) => i + 1);
-                    console.log(this.totalPagesObject)
-                }
-                nProgress.done();
-                this.isLoading = false;
-            });
+
+    setCurrentPage(page) {
+      this.currentPage = page;
+      this.getReviewsList(true);
     },
-    onPrivateContentLoaded(data: any) {
-        this.customerToken = data.customer?.signin_token || false;
-        if (this.customerToken) {
-            this.getReviewsList();
-        } else {
-            nProgress.done();
-            this.isLoading = false;
-        }
+    getCustomerReviewsQuery() {
+      return props.customerReviewsQuery
+        .replace("%currentPage%", this.currentPage.toString())
+        .replace("%pageSize%", this.pageSize.toString());
     },
-    initErrorMessages(errors: any) {
-        this.errors = errors.map((error: { message: string }) => error.message);
+    getReviewsList(append = false) {
+      this.isLoading = true;
+      nProgress.start();
+      return fetch(`${BASE_URL}graphql`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Store: props.storeCode,
+        },
+        credentials: "include",
+        body: JSON.stringify({
+          query: this.getCustomerReviewsQuery(),
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data && data.errors) {
+            this.initErrorMessages(data.errors);
+          } else {
+            const newReviews =
+              (data &&
+                data.data &&
+                data.data.customer &&
+                data.data.customer.reviews &&
+                data.data.customer.reviews.items) ||
+              [];
+
+            this.reviews = append
+              ? this.reviews.concat(newReviews)
+              : newReviews;
+
+            this.pageInfo =
+              (data &&
+                data.data &&
+                data.data.customer &&
+                data.data.customer.reviews &&
+                data.data.customer.reviews.page_info) ||
+              [];
+            this.totalPagesObject =
+              (this.pageInfo &&
+                this.pageInfo.total_pages &&
+                Array.from(
+                  { length: this.pageInfo.total_pages },
+                  (v, i) => i + 1,
+                )) ||
+              [];
+          }
+          this.isLoading = false;
+          nProgress.done();
+        });
     },
-}
+    initErrorMessages(errors) {
+      this.errors = errors.map((error: any) => error.message);
+    },
+  };
