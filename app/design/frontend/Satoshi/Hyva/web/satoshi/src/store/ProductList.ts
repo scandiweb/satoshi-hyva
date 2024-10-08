ca43b52bf92e2ee890eb911612c969dabb76e748
Nika Beridze <nika.beridze@scandiweb.com>
2024-10-08 06:49:22 +0400

    INTERNAL-300: Implement hide button behaviour fix

--- app/design/frontend/Satoshi/Hyva/web/satoshi/src/store/ProductList.ts
+++ app/design/frontend/Satoshi/Hyva/web/satoshi/src/store/ProductList.ts
@@ -1,15 +1,24 @@
-import type { Magics } from "alpinejs";
+import type {Magics} from "alpinejs";
 
 export type ProductListStoreType = {
-  isFiltersSidebarExpanded: boolean;
+    isFiltersSidebarExpanded: boolean;
 
-  toggleFiltersSidebar(): void;
+    toggleFiltersSidebar(): void;
 } & Magics<{}>;
 
 export const ProductListStore = <ProductListStoreType>{
-  isFiltersSidebarExpanded: true,
+    isFiltersSidebarExpanded: true,
 
-  toggleFiltersSidebar() {
-    this.isFiltersSidebarExpanded = !this.isFiltersSidebarExpanded;
-  },
+    toggleFiltersSidebar() {
+        this.isFiltersSidebarExpanded = !this.isFiltersSidebarExpanded;
+        const element = document.querySelector('.grid-columns');
+
+        if (this.isFiltersSidebarExpanded) {
+            element.className = element.className.replace('md:gap-0', 'md:gap-8');
+        } else {
+            if (element) {
+                element.className = element.className.replace('md:gap-8', 'md:gap-0');
+            }
+        }
+    },
 };
