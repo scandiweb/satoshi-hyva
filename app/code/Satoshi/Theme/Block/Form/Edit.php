00f6cf7ab629662f3a993e2bb21507541a1610b0
Mohamed Idris <mohamed.idris@scandiweb.com>
2024-10-08 20:30:42 +0300

    INTERNAL-17; implement error msg based on session

--- app/code/Satoshi/Theme/Block/Form/Edit.php
+++ app/code/Satoshi/Theme/Block/Form/Edit.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace Satoshi\Theme\Block\Form;
 
@@ -7,26 +8,26 @@ use Magento\Customer\Block\Form\Edit as SourceEdit;
 class Edit extends SourceEdit
 {
     /**
-     * Retrieve and clear the error messages from the session
-     *
-     * @return string|null
-     */
-    public function getErrorMessage(): ?string
-    {
-        $errorMessage = $this->customerSession->getErrorMessage();
-        $this->customerSession->unsErrorMessage();
-        return $errorMessage;
-    }
-
-    /**
      * Retrieve and clear the success message from the session
      *
      * @return string|null
      */
-    public function getSuccessMessage(): ?string
+    public function getSuccessMessage()
     {
         $successMessage = $this->customerSession->getSuccessMessage();
         $this->customerSession->unsSuccessMessage();
         return $successMessage;
+    }
+
+    /**
+     * Retrieve and clear the error message from the session
+     *
+     * @return array|null
+     */
+    public function getErrorMessage()
+    {
+        $errorMessage = $this->customerSession->getErrorMessage();
+        $this->customerSession->unsErrorMessage();
+        return $errorMessage;
     }
 }
