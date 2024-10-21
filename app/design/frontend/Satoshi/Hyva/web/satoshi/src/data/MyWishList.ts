47bd02c0630e267ecf73ee4ec3c899b96dab97bc
Mohamed Idris <mohamed.idris@scandiweb.com>
2024-10-21 11:27:44 +0300

    INTERNAL-370; add postFormWithRedirect method to prevent page reload

--- app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/MyWishList.ts
+++ app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/MyWishList.ts
@@ -1,9 +1,16 @@
 import { Magics } from "alpinejs";
 
+export type PostParams = {
+  action: string;
+  data: Record<string, string>;
+  skipUenc?: boolean;
+};
+
 export type MyWishListType = {
   isLoading: boolean;
   addToCart(productId: string, postParams: any): void;
   addAllItemsToCart(): void;
+  postFormWithRedirect(postParams: PostParams): void;
 } & Magics<{}>;
 
 export const MyWishList = (
@@ -11,22 +18,60 @@ export const MyWishList = (
 ) =>
     <MyWishListType>{
       isLoading: false,
-    addToCart(productId, postParams) {
-      const qtyInput = document.querySelector<HTMLInputElement>(`[name="qty[${productId}]"]`);
-      postParams.data.qty = qtyInput ? qtyInput.value : postParams.data.qty;
+      addToCart(productId, postParams) {
+        const qtyInput = this.$refs[`product-qty-${productId}`] as HTMLInputElement | null;
+        postParams.data.qty = qtyInput ? qtyInput.value : postParams.data.qty;
 
-      window.hyva.postForm(postParams);
-    },
+        this.postFormWithRedirect(postParams);
+      },
 
-    addAllItemsToCart() {
-      let separator = urlParams.action.indexOf('?') >= 0 ? '&' : '?';
+      addAllItemsToCart() {
+        let separator = urlParams.action.indexOf('?') >= 0 ? '&' : '?';
 
-      Array.from(document.querySelectorAll('input[name^=qty]')).map((qty) => {
-        const inputElement = qty as HTMLInputElement;
-        urlParams.action += separator + inputElement.name + '=' + encodeURIComponent(inputElement.value);
-        separator = '&';
-      });
+        Array.from(document.querySelectorAll('input[name^=qty]')).map((qty) => {
+          const inputElement = qty as HTMLInputElement;
+          urlParams.action += separator + inputElement.name + '=' + encodeURIComponent(inputElement.value);
+          separator = '&';
+        });
 
-      window.hyva.postForm(urlParams);
-    },
-};
+        this.postFormWithRedirect(urlParams);
+      },
+
+      postFormWithRedirect(postParams) {
+        const form = document.createElement("form");
+
+        let data = postParams.data;
+
+        if (!postParams.skipUenc && !data.uenc) {
+          data.uenc = btoa(window.location.href);
+        }
+        form.method = "POST";
+        form.action = postParams.action;
+
+        Object.keys(postParams.data).map(key => {
+          const field = document.createElement("input");
+          field.type = 'hidden'
+          field.value = postParams.data[key];
+          field.name = key;
+          form.appendChild(field);
+        });
+
+        const form_key = document.createElement("input");
+        form_key.type = 'hidden';
+        form_key.value = window.hyva.getFormKey();
+        form_key.name = "form_key";
+        form.appendChild(form_key);
+
+        fetch(form.action, {
+          method: "POST",
+          body: new FormData(form),
+        }).then(async (res) => {
+          if (res.ok) {
+            window.hyva.replaceDomElement("#MainContent", await res.text());
+          }
+        }).catch((error) => {
+          console.error("Error while form submission", error);
+          location.reload();
+        });
+      },
+    };
