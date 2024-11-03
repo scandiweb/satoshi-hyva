e1b0f8b1cbe9b422ec6bb26f16036fc8049cb199
sh1r3f <sh1r3f@gmail.com>
2024-11-03 23:40:10 +0200

    INTERNAL-17: Refactor recaptcha and optimize validation

--- app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/AccountInformation.ts
+++ app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/AccountInformation.ts
@@ -1,93 +1,29 @@
 import { Magics } from "alpinejs";
-import { replaceMainContentWithTransition } from "../plugins/Transition";
+import { FormType } from "./Form";
 
 export type AccountInformationType = {
-  isLoading: boolean;
   showEmailField: boolean;
   showPasswordFields: boolean;
-  errorMessages: string[];
-  errors: number;
-  recaptchaErrorMessage?: string;
-  validate(): Promise<void>;
   handleCheckboxChange(checkboxId: string): void;
-  setErrorMessages(messages: string[]): void;
-  saveChanges(form: HTMLFormElement): void;
-  validateRecaptcha(form: HTMLFormElement): void;
-  submitForm(event: Event): void;
-} & Magics<{}>;
+} & FormType &
+  Magics<{}>;
 
 export const AccountInformation = (
-    recaptchaValidationScript: Function,
-    initialShowEmailField: boolean,
-    initialShowPasswordFields: boolean,
+  initialShowEmailField: boolean,
+  initialShowPasswordFields: boolean,
 ) =>
-    <AccountInformationType>{
-      isLoading: false,
-      showEmailField: initialShowEmailField || false,
-      showPasswordFields: initialShowPasswordFields || false,
-      errorMessages: [] as string[],
-      errors: 0,
-      recaptchaErrorMessage: undefined,
+  <AccountInformationType>{
+    showEmailField: !!initialShowEmailField,
+    showPasswordFields: !!initialShowPasswordFields,
 
-      handleCheckboxChange(checkboxId) {
-        this.$nextTick(() => {
-          const firstFocusableElement = document.querySelector(`#${checkboxId} input, #${checkboxId} textarea, #${checkboxId} select`) as HTMLElement;
-          if (firstFocusableElement) {
-            firstFocusableElement.focus();
-          }
-        });
-      },
-
-      setErrorMessages(messages: string[]) {
-        this.errorMessages = messages;
-      },
-
-      saveChanges($form) {
-        if (!$form) return;
-
-        const formData = new FormData($form);
-        this.isLoading = true;
-
-        fetch($form.action, {
-          method: "POST",
-          body: formData,
-        }).then(async (res) => {
-          if (res.ok) {
-            await replaceMainContentWithTransition(res.url, await res.text());
-          }
-        }).catch((error) => {
-          console.error("Error while updating account information:", error);
-          location.reload();
-        }).finally(() => {
-          this.isLoading = false;
-        });
-      },
-
-      validateRecaptcha(form: HTMLFormElement) {
-        recaptchaValidationScript();
-        const {errors, errorMessage} = window.validateRecaptchaToken(form);
-
-        this.errors = errors ? errors : 0;
-        this.recaptchaErrorMessage = errors ? errorMessage : undefined;
-      },
-
-      submitForm(event) {
-        this.validate()
-            .then(() => {
-              const $form = event.target as HTMLFormElement;
-
-              if (recaptchaValidationScript) {
-                this.validateRecaptcha($form);
-              }
-
-              if (this.errors === 0) {
-                this.saveChanges($form);
-              }
-            })
-            .catch((invalid) => {
-              if (invalid.length > 0) {
-                (invalid[0] as HTMLElement).focus();
-              }
-            });
-      },
-    };
+    handleCheckboxChange(checkboxId) {
+      this.$nextTick(() => {
+        const firstFocusableElement = document.querySelector(
+          `#${checkboxId} input, #${checkboxId} textarea, #${checkboxId} select`,
+        ) as HTMLElement;
+        if (firstFocusableElement) {
+          firstFocusableElement.focus();
+        }
+      });
+    },
+  };
