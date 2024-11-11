cd81eda5be59b36903fe94c323a5248ecc74b93f
Mohamed Idris <edriso.dev@edriso.com>
2024-11-11 12:10:19 +0200

    INTERNAL-370: fix indentation

--- app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/WishListPage.ts
+++ app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/WishListPage.ts
@@ -18,106 +18,106 @@ export type WishListPageType = {
 } & Magics<{}>;
 
 export const WishListPage = (
-    urlParams: { action: string, data: any },
+  urlParams: { action: string, data: any },
 ) =>
-    <WishListPageType>{
-      isLoading: false,
-      actionBtnText: '',
+  <WishListPageType>{
+    isLoading: false,
+    actionBtnText: '',
 
-      addToCart(itemId, postParams, productSku) {
-        const qtyInput = this.$refs[`product-qty-${itemId}`] as HTMLInputElement | null;
-        postParams.data.qty = qtyInput?.value ?? postParams.data.qty;
+    addToCart(itemId, postParams, productSku) {
+      const qtyInput = this.$refs[`product-qty-${itemId}`] as HTMLInputElement | null;
+      postParams.data.qty = qtyInput?.value ?? postParams.data.qty;
 
-        this.postFormWithRedirect(postParams, [productSku]);
-      },
+      this.postFormWithRedirect(postParams, [productSku]);
+    },
 
-      addAllItemsToCart() {
-        let separator = urlParams.action.indexOf('?') >= 0 ? '&' : '?';
-        const addedProductSkus = [] as string[];
+    addAllItemsToCart() {
+      let separator = urlParams.action.indexOf('?') >= 0 ? '&' : '?';
+      const addedProductSkus = [] as string[];
 
-        document.querySelectorAll('input[name^=qty]').forEach((inputElement) => {
-          const input = inputElement as HTMLInputElement;
-          urlParams.action += separator + input.name + '=' + encodeURIComponent(input.value);
-          separator = '&';
+      document.querySelectorAll('input[name^=qty]').forEach((inputElement) => {
+        const input = inputElement as HTMLInputElement;
+        urlParams.action += separator + input.name + '=' + encodeURIComponent(input.value);
+        separator = '&';
 
-          const productSku = input.getAttribute('data-product-sku') as string;
-          if (productSku) {
-            addedProductSkus.push(productSku);
+        const productSku = input.getAttribute('data-product-sku') as string;
+        if (productSku) {
+          addedProductSkus.push(productSku);
+        }
+      });
+
+      this.postFormWithRedirect(urlParams, addedProductSkus);
+    },
+
+    postFormWithRedirect(postParams, addedProductSkus = [] as string[]) {
+      this.isLoading = true;
+      const form = document.createElement("form");
+
+      let data = postParams.data;
+
+      if (!postParams.skipUenc && !data.uenc) {
+        data.uenc = btoa(window.location.href);
+      }
+      form.method = "POST";
+      form.action = postParams.action;
+
+      Object.keys(postParams.data).map(key => {
+        const field = document.createElement("input");
+        field.type = 'hidden'
+        field.value = postParams.data[key];
+        field.name = key;
+        form.appendChild(field);
+      });
+
+      const form_key = document.createElement("input");
+      form_key.type = 'hidden';
+      form_key.value = window.hyva.getFormKey();
+      form_key.name = "form_key";
+      form.appendChild(form_key);
+
+      fetch(form.action, {
+        method: "POST",
+        body: new FormData(form),
+      }).then(async (res) => {
+        if (res.ok) {
+          window.hyva.replaceDomElement("#MainContent", await res.text());
+          if (addedProductSkus.length) {
+            this.focusOnCartAddedItems(addedProductSkus);
           }
-        });
-
-        this.postFormWithRedirect(urlParams, addedProductSkus);
-      },
-
-      postFormWithRedirect(postParams, addedProductSkus = [] as string[]) {
-        this.isLoading = true;
-        const form = document.createElement("form");
-
-        let data = postParams.data;
-
-        if (!postParams.skipUenc && !data.uenc) {
-          data.uenc = btoa(window.location.href);
         }
-        form.method = "POST";
-        form.action = postParams.action;
+      }).catch((error) => {
+        console.error("Error while form submission", error);
+        location.reload();
+      }).finally(() => {
+        this.isLoading = false;
+        this.onActionBtnChange();
+      });
+    },
 
-        Object.keys(postParams.data).map(key => {
-          const field = document.createElement("input");
-          field.type = 'hidden'
-          field.value = postParams.data[key];
-          field.name = key;
-          form.appendChild(field);
-        });
+    focusOnCartAddedItems(addedProductSkus = [] as string[]) {
+      const cartItems = Alpine.store("cart").cartItems;
 
-        const form_key = document.createElement("input");
-        form_key.type = 'hidden';
-        form_key.value = window.hyva.getFormKey();
-        form_key.name = "form_key";
-        form.appendChild(form_key);
+      const itemIds = cartItems
+        .filter((item: CartItem) => addedProductSkus.includes(item.product_sku))
+        .map((item) => item.item_id);
 
-        fetch(form.action, {
-          method: "POST",
-          body: new FormData(form),
-        }).then(async (res) => {
-          if (res.ok) {
-            window.hyva.replaceDomElement("#MainContent", await res.text());
-            if (addedProductSkus.length) {
-              this.focusOnCartAddedItems(addedProductSkus);
-            }
-          }
-        }).catch((error) => {
-          console.error("Error while form submission", error);
-          location.reload();
-        }).finally(() => {
-          this.isLoading = false;
-          this.onActionBtnChange();
-        });
-      },
+      if (itemIds.length) {
+        Alpine.store("cart").focusInCart(itemIds);
+      } else {
+        Alpine.store("cart").showCart();
+      }
+    },
 
-      focusOnCartAddedItems(addedProductSkus = [] as string[]) {
-        const cartItems = Alpine.store("cart").cartItems;
+    onActionBtnChange(text) {
+      this.actionBtnText = text || '';
 
-        const itemIds = cartItems
-            .filter((item: CartItem) => addedProductSkus.includes(item.product_sku))
-            .map((item) => item.item_id);
+      const updateOrShareInput = document.getElementById('update-or-share');
+      if (!updateOrShareInput || !text) return;
 
-        if (itemIds.length) {
-          Alpine.store("cart").focusInCart(itemIds);
-        } else {
-          Alpine.store("cart").showCart();
-        }
-      },
-
-      onActionBtnChange(text) {
-        this.actionBtnText = text || '';
-
-        const updateOrShareInput = document.getElementById('update-or-share');
-        if (!updateOrShareInput || !text) return;
-
-        if (text === 'share-wish-list') {
-          updateOrShareInput.setAttribute('name', 'save_and_share');
-        } else if (text === 'update-wish-list') {
-          updateOrShareInput.setAttribute('name', 'do');
-        }
-      },
-    };
+      if (text === 'share-wish-list') {
+        updateOrShareInput.setAttribute('name', 'save_and_share');
+      } else if (text === 'update-wish-list') {
+        updateOrShareInput.setAttribute('name', 'do');
+      }
+    },
+  };
