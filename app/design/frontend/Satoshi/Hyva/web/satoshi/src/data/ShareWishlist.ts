b584c809331744147220254610d891c2fd2fc053
Mohamed Idris <edriso.dev@edriso.com>
2024-11-13 13:20:19 +0200

    INTERNAL-369; fix indentation

--- app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/ShareWishlist.ts
+++ app/design/frontend/Satoshi/Hyva/web/satoshi/src/data/ShareWishlist.ts
@@ -1,4 +1,4 @@
-import {Magics} from "alpinejs";
+import { Magics } from "alpinejs";
 
 export type ShareWishlistType = {
   isLoading: boolean;
@@ -8,35 +8,35 @@ export type ShareWishlistType = {
 } & Magics<{}>;
 
 export const ShareWishlist = (
-    emailSharingLimit: number,
-    invalidEmailMessage: string,
-    emailLimitMessage: string,
+  emailSharingLimit: number,
+  invalidEmailMessage: string,
+  emailLimitMessage: string,
 ) =>
-    <ShareWishlistType>{
-      isLoading: false,
-      isValid: true,
-      validateEmailsRegex:
-          /^([a-z0-9,!\#\$%&'\*\+\/=\?\^_`\{\|\}~-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z0-9,!\#\$%&'\*\+\/=\?\^_`\{\|\}~-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*@([a-z0-9-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z0-9-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*\.(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]){2,})$/i,
+  <ShareWishlistType>{
+    isLoading: false,
+    isValid: true,
+    validateEmailsRegex:
+      /^([a-z0-9,!\#\$%&'\*\+\/=\?\^_`\{\|\}~-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z0-9,!\#\$%&'\*\+\/=\?\^_`\{\|\}~-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*@([a-z0-9-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z0-9-]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*\.(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]){2,})$/i,
 
-      validateForm() {
-        const emailField = this.$refs["emails"] as HTMLTextAreaElement;
-        const emails = emailField.value.split(/[\s\n\,]+/g);
+    validateForm() {
+      const emailField = this.$refs["emails"] as HTMLTextAreaElement;
+      const emails = emailField.value.split(/[\s\n\,]+/g);
 
-        for (let i = 0; i < emails.length; i++) {
-          if (!this.validateEmailsRegex.test(emails[i].trim())) {
-            this.isValid = false;
-            emailField.setCustomValidity(invalidEmailMessage);
-            return false;
-          }
-        }
-
-        if (emails.length > emailSharingLimit) {
-          emailField.setCustomValidity(emailLimitMessage);
+      for (let i = 0; i < emails.length; i++) {
+        if (!this.validateEmailsRegex.test(emails[i].trim())) {
+          this.isValid = false;
+          emailField.setCustomValidity(invalidEmailMessage);
           return false;
         }
+      }
 
-        this.isValid = true;
-        emailField.setCustomValidity("");
-        return true;
-      },
-    };
+      if (emails.length > emailSharingLimit) {
+        emailField.setCustomValidity(emailLimitMessage);
+        return false;
+      }
+
+      this.isValid = true;
+      emailField.setCustomValidity("");
+      return true;
+    },
+  };
