53b703fd555953cd6f7ced544d71c358bf290128
Mahmoud Shiref <sh1r3f@gmail.com>
2024-12-24 22:40:50 +0200

    INTERNAL-371: Implement non-anchor category filters

--- app/code/Satoshi/Theme/Block/Popup.php
+++ app/code/Satoshi/Theme/Block/Popup.php
@@ -2,41 +2,78 @@
 
 namespace Satoshi\Theme\Block;
 
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\View\Element\AbstractBlock;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 use Magento\Framework\View\Element\Text;
 
 class Popup extends Template
 {
-  protected $_template = 'Satoshi_Theme::popup.phtml';
+    /**
+     * @var string
+     */
+    protected $_template = 'Satoshi_Theme::popup.phtml';
 
-  public function __construct(
-    Context $context,
-    array $data = []
-  ) {
-    parent::__construct($context, $data);
-  }
+    /**
+     * @param Context $context
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        array   $data = []
+    )
+    {
+        parent::__construct($context, $data);
+    }
 
-  public function setChildTemplate(string $childTemplate, array $data = [])
-  {
-    $this->setChild(
-      'popup-content',
-      $this->getLayout()->createBlock(Template::class)->setTemplate($childTemplate)->setData($data)
-    );
-    return $this;
-  }
+    /**
+     * @param string $childTemplate
+     * @param array $data
+     * @return $this
+     * @throws LocalizedException
+     */
+    public function setChildTemplate(string $childTemplate, array $data = [])
+    {
+        $this->setChild(
+            'popup-content',
+            $this->getLayout()->createBlock(Template::class)->setTemplate($childTemplate)->setData($data)
+        );
+        return $this;
+    }
 
-  public function setChildHtml(string $childHtml)
-  {
-    $this->setChild(
-      'popup-content',
-      $this->getLayout()->createBlock(Text::class)->setText($childHtml)
-    );
-    return $this;
-  }
+    /**
+     * @param AbstractBlock|string $childBlock
+     * @return $this
+     */
+    public function setChildBlock(AbstractBlock|string $childBlock)
+    {
+        $this->setChild(
+            'popup-content',
+            $childBlock
+        );
+        return $this;
+    }
 
-  public function render()
-  {
-    return $this->toHtml();
-  }
+    /**
+     * @param string $childHtml
+     * @return $this
+     * @throws LocalizedException
+     */
+    public function setChildHtml(string $childHtml)
+    {
+        $this->setChild(
+            'popup-content',
+            $this->getLayout()->createBlock(Text::class)->setText($childHtml)
+        );
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function render()
+    {
+        return $this->toHtml();
+    }
 }
