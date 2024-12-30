b857752e6b7164b5203aa775bab43ebbb7fa424a
Mohamed Idris <edriso.dev@edriso.com>
2024-12-30 10:27:43 +0200

    INTERNAL-411-162; fix the issue from the phtml file

--- app/design/frontend/Satoshi/Hyva/web/satoshi/src/plugins/Accessibility.ts
+++ app/design/frontend/Satoshi/Hyva/web/satoshi/src/plugins/Accessibility.ts
@@ -190,11 +190,7 @@ export default function (Alpine: AlpineType) {
       if (
         !e.target ||
         !(e.target instanceof HTMLElement) ||
-        (
-          e.target.tagName === "INPUT"
-            ? e.target.parentElement?.role !== "radio"
-            : e.target.role !== "radio"
-        )
+        e.target.role !== "radio"
       ) {
         throw "x-a11y-radio can only be used on elements of 'radio' role";
       }
