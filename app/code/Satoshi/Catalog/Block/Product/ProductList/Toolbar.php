b0ad755bda421c5c3a31766ce715f2488a308820
Reza Mohseni <reza.mohseni@scandiweb.com>
2025-02-07 16:13:35 +0330

    INTERNAL-495 - Add progress bar to the load more button

---
+++ app/code/Satoshi/Catalog/Block/Product/ProductList/Toolbar.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Satoshi\Catalog\Block\Product\ProductList;
+
+use Magento\Catalog\Block\Product\ProductList\Toolbar as SourceToolbar;
+
+class Toolbar extends SourceToolbar
+{
+    /**
+     * Render pagination HTML
+     * Overriding to use setIsProductList(true) function
+     *
+     * @return string
+     */
+    public function getPagerHtml()
+    {
+        $pagerBlock = $this->getChildBlock('product_list_toolbar_pager');
+
+        if ($pagerBlock instanceof \Magento\Framework\DataObject) {
+            /* @var $pagerBlock \Magento\Theme\Block\Html\Pager */
+            $pagerBlock->setAvailableLimit($this->getAvailableLimit());
+
+            $pagerBlock->setUseContainer(
+                false
+            )->setShowPerPage(
+                false
+            )->setShowAmounts(
+                false
+            )->setFrameLength(
+                $this->_scopeConfig->getValue(
+                    'design/pagination/pagination_frame',
+                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+                )
+            )->setJump(
+                $this->_scopeConfig->getValue(
+                    'design/pagination/pagination_frame_skip',
+                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+                )
+            )->setLimit(
+                $this->getLimit()
+            )->setCollection(
+                $this->getCollection()
+            )->setIsProductList(
+                true
+            );
+
+            return $pagerBlock->toHtml();
+        }
+
+        return '';
+    }
+}
